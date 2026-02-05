d111dfc9436091885555fd134a1eb9bb12cc5dfc
ayoub heragua <70757644+herra12@users.noreply.github.com>
2026-02-05 18:55:32 +0100

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,31 @@
+SELECT COUNT(*)
+FROM `purchasingdb.purchasing_dataset_project.achats `
+LIMIT 1000;
+
+SELECT COUNT(DISTINCT SupplierID)
+FROM `purchasingdb.purchasing_dataset_project.achats `;
+
+
+SELECT * FROM `purchasingdb.purchasing_dataset_project.achats `;
+
+SELECT ItemID , SUM(Quantity) AS Total_Quantity
+FROM `purchasingdb.purchasing_dataset_project.achats `
+WHERE Mois IN ('janvier', 'février', 'mars', 'avril')
+GROUP BY ItemID;
+
+SELECT ItemID  , Buyer , SUM(Quantity*UnitPrice)  as SUMQUENTUNIT
+FROM `purchasingdb.purchasing_dataset_project.achats `
+GROUP BY ItemID , Buyer
+ORDER BY SUMQUENTUNIT; 
+
+SELECT i.category,p.buyer, SUM(p.Quantity * p.UnitPrice) AS totalcost
+FROM `purchasingdb.purchasing_dataset_project.achats `AS p
+JOIN  `purchasingdb.purchasing_dataset_project.Item ` AS i
+ON p.ItemID = i.ItemID
+GROUP BY i.category , p.buyer
+ORDER BY totalcost;
+
+
+
+
 
